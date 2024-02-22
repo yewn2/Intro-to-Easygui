c07a8b5a28060683230cd3fb57233eb203780377
yewn2 <yewn2@middleton.school.nz>
2024-02-23 11:25:50 +1300

    favourite_places version 1

---
+++ favourite_places_v1.py
@@ -0,0 +1,5 @@
+places = ["Italy", "Japan", "Germany"]
+
+print(f"The place I would most like to visit is {places[0]}")
+print(f"My second choice would be {places[1]}")
+print(f"My third choice would be {places[2]}")
