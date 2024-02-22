b084590e54c9dfaef3393fa50b8db2839796eed4
yewn2 <yewn2@middleton.school.nz>
2024-02-23 11:27:29 +1300

    favourite_places version 2

--- favourite_places_v2.py
+++ favourite_places_v2.py
@@ -3,9 +3,9 @@ import easygui
 name = easygui.enterbox("What is your name?", "Name")
 pronoun = easygui.buttonbox("What is your preferred pronoun?",
                             "Pronoun", ["his", "her", "their"])
-places = [easygui.enterbox("What is your most favourite place to visit?"),
-          easygui.enterbox("What is your second favourite?"),
-          easygui.enterbox("What is your third favourite?")]
+places = [easygui.enterbox("What is the place you would most like to visit?"),
+          easygui.enterbox("Pick a second place?"),
+          easygui.enterbox("Pick a third place?")]
 
 print(f"The place {name} would most like to visit is {places[0]}")
 print(f"{pronoun} second choice would be {places[1]}")
