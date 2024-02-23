704def0d734ce3df98aad8dbbfbce6b557959f56
yewn2 <yewn2@middleton.school.nz>
2024-02-23 20:25:57 +1300

    testing

--- test.py
+++ test.py
@@ -14,17 +14,17 @@ print(letter)
 
 print(friend[3])
 
-# name = easygui.enterbox("Hi, what's your name?", "Name")
-# print(name)
-#
-# age = easygui.integerbox("How old are you?")
-# print(age)
-#
-# carry_on = easygui.buttonbox("Do you want to continue?",
-#                              choices=["Yes", "No", "Don't care"],
-#                              title="Continue")
-#
-# welcome = easygui.msgbox("Welcome to Easygui", title="Welcome")
-#
-# gender = easygui.buttonbox("Choose gender", "Gender",
-#                            ["man", "woman", "other"])
+name = easygui.enterbox("Hi, what's your name?", "Name")
+print(name)
+
+age = easygui.integerbox("How old are you?")
+print(age)
+
+carry_on = easygui.buttonbox("Do you want to continue?",
+                             choices=["Yes", "No", "Don't care"],
+                             title="Continue")
+
+welcome = easygui.msgbox("Welcome to Easygui", title="Welcome")
+
+gender = easygui.buttonbox("Choose gender", "Gender",
+                           ["man", "woman", "other"])
