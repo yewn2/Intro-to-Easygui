0a1d26bd0aed4ef2e3f3179d953f4f74aae82b7d
yewn2 <yewn2@middleton.school.nz>
2024-03-07 17:31:08 +1300

    roll_dice_v1

---
+++ Introduction to Easygui/roll_dice_v1.py
@@ -0,0 +1,11 @@
+import easygui
+import random
+
+player = 0
+
+for roll in range(2):
+    player += 1
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
+    easygui.msgbox(f"Player {player} you rolled:\n\n{die1} and {die2}\n\n"
+                   f"Total: {die1 + die2}", f"Player {player}")
