b76d003a4725cd4c05253834a9b5d3a44db25dcb
yewn2 <yewn2@middleton.school.nz>
2024-03-07 17:30:59 +1300

    rock_paper_scissors_v1

---
+++ Introduction to Easygui/rock_paper_scissors_v1.py
@@ -0,0 +1,34 @@
+import easygui
+import random
+
+welcome = easygui.buttonbox("Welcome to Rock Paper Scissors\n"
+                            "Rock beats Scissors\n"
+                            "Paper beats Rock\n"
+                            "Scissors beats Paper\n\n"
+                            "Do you want to play?", "Welcome and Rules",
+                            ["Yes", "No"])
+while welcome == "Yes":
+    weapons = ["Rock", "Paper", "Scissors"]
+    playername = easygui.enterbox("Enter your name", "Name")
+    my_choice = easygui.buttonbox("Choose your weapon", "Weapon choice",
+                                  [weapons[0], weapons[1], weapons[2]])
+    pc_choice = random.choice(weapons)
+    choice = easygui.msgbox(f"{playername} chose {my_choice} and the computer "
+                            f"chose {pc_choice}")
+    if pc_choice == my_choice:
+        easygui.msgbox("This was a draw")
+    elif ((pc_choice == weapons[0] and my_choice == weapons[1]) or
+          (pc_choice == weapons[1] and my_choice == weapons[2]) or
+          (pc_choice == weapons[2] and my_choice == weapons[0])):
+        easygui.msgbox("You win")
+    elif ((pc_choice == weapons[0] and my_choice == weapons[2]) or
+          (pc_choice == weapons[1] and my_choice == weapons[0]) or
+          (pc_choice == weapons[2] and my_choice == weapons[1])):
+        easygui.msgbox("Computer wins")
+
+    welcome = easygui.buttonbox("Welcome to Rock Paper Scissors\n"
+                                "Rock beats Scissors\n"
+                                "Paper beats Rock\n"
+                                "Scissors beats Paper\n\n"
+                                "Do you want to play?", "Welcome and Rules",
+                                ["Yes", "No"])
