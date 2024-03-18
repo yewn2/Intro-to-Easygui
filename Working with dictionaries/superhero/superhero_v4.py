d0cacefe61cff7c53bf5dcfff8254801ae22d2a0
yewn2 <yewn2@middleton.school.nz>
2024-03-19 10:05:49 +1300

    superhero_v4

---
+++ Working with dictionaries/superhero/superhero_v4.py
@@ -0,0 +1,17 @@
+hero_0 = {
+    "name": "Materdon",
+    "power": "flight",
+    "strength": "5"
+}
+
+print(hero_0)
+
+hero_0["speed"] = 12
+print(hero_0)
+
+hero_0["strength"] = 6
+print(hero_0)
+
+popped_item = hero_0.pop("power")
+print(hero_0)
+print(popped_item)
