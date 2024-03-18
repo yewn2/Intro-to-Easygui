ad29eb6cf75d494ecd8e420507e92202274a6553
yewn2 <yewn2@middleton.school.nz>
2024-03-19 10:30:30 +1300

    login_number

---
+++ Working with dictionaries/login_number.py
@@ -0,0 +1,27 @@
+import easygui
+
+
+def login_number():
+    logins = {
+        "window": "cross5",
+        "guest": "12345",
+        "superhero": "hero1",
+        "new_login": "password"
+    }
+
+    username = easygui.enterbox("Enter your username: ", "User")
+
+    while username not in logins:
+        easygui.msgbox("Incorrect username, please try again.")
+        username = easygui.enterbox("Enter your username: ", "User")
+
+    password = easygui.passwordbox(f"Enter the password for user {username}:",
+                                   "Password")
+
+    if password == logins[username]:
+        easygui.msgbox(f"Welcome {username}", "Correct password")
+    else:
+        easygui.msgbox("Sorry, wrong password", "Incorrect password")
+
+
+login_number()
